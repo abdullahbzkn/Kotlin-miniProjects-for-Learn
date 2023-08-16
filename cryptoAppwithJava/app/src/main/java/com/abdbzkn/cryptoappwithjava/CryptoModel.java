e9b9ef686a981ba68203b71c0364f4b88ea0ad52
Abdullah Buzkan <abdbzkn65@gmail.com>
2023-08-16 10:47:57 +0300

    added cryptoApp mProject

---
+++ cryptoAppwithJava/app/src/main/java/com/abdbzkn/cryptoappwithjava/CryptoModel.java
@@ -0,0 +1,9 @@
+package com.abdbzkn.cryptoappwithjava;
+
+import com.google.gson.annotations.SerializedName;
+public class CryptoModel {
+@SerializedName("currency")
+    public String currency;
+@SerializedName("price")
+    public String price;
+}
