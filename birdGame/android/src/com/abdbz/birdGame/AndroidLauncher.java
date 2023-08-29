08e9eda535078db412aafac86f2a80ce77303b77
Abdullah Buzkan <abdbzkn65@gmail.com>
2023-08-29 09:45:46 +0300

    updated birdGame

--- birdGame/android/src/com/abdbz/birdGame/AndroidLauncher.java
+++ birdGame/android/src/com/abdbz/birdGame/AndroidLauncher.java
@@ -1,10 +1,8 @@
 package com.abdbz.birdGame;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
